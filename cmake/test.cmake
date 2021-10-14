3a715c8af74fc8c149b7f2c3eaf90d8ebf47e081
devcareer00 <fldevcareer0@gmail.com>
2021-10-14 20:19:28 +0200

    Initial cmake build structure

---
+++ cmake/test.cmake
@@ -0,0 +1,39 @@
+function(disable_clang_tidy target)
+  set_target_properties(${target} PROPERTIES
+      C_CLANG_TIDY ""
+      CXX_CLANG_TIDY ""
+      )
+endfunction()
+
+function(addtest test_name)
+  add_executable(${test_name} ${ARGN})
+  addtest_part(${test_name} ${ARGN})
+  target_link_libraries(${test_name}
+      GTest::gtest_main
+      GTest::gmock_main
+      )
+  file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/xunit)
+  set(xml_output "--gtest_output=xml:${CMAKE_BINARY_DIR}/xunit/xunit-${test_name}.xml")
+  add_test(
+      NAME ${test_name}
+      COMMAND $<TARGET_FILE:${test_name}> ${xml_output}
+  )
+  set_target_properties(${test_name} PROPERTIES
+      RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/test_bin
+      ARCHIVE_OUTPUT_PATH ${CMAKE_BINARY_DIR}/test_lib
+      LIBRARY_OUTPUT_PATH ${CMAKE_BINARY_DIR}/test_lib
+      )
+  disable_clang_tidy(${test_name})
+endfunction()
+
+function(addtest_part test_name)
+  if (POLICY CMP0076)
+    cmake_policy(SET CMP0076 NEW)
+  endif ()
+  target_sources(${test_name} PUBLIC
+      ${ARGN}
+      )
+  target_link_libraries(${test_name}
+      GTest::gtest
+      )
+endfunction()
